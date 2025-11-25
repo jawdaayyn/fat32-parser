85a812992969695ba18fdfed07d167126421e13c
Jawdan <akk.jawdanpro@gmail.com>
2025-11-25 14:24:46 +0100

    added isEoc fn

--- src/fat.rs
+++ src/fat.rs
@@ -3,3 +3,8 @@ pub const FAT_FREE: u32 = 0x00000000;
 pub const FAT_BAD: u32 = 0x0FFFFFF7;
 pub const FAT_EOC: u32 = 0x0FFFFFF8;
 
+/// vérifie si un cluster est la fin de chaîne
+pub fn is_eoc(cluster: u32) -> bool {
+    cluster >= FAT_EOC
+}
+
